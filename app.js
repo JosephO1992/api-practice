c070d820c5c9e43a889ac14d0e8c3e1d847bf2e1
Joe <josephraoreilly@gmail.com>
2020-12-27 17:01:32 +0800

    Changed to async and updated HTML with data

--- app.js
+++ app.js
@@ -5,36 +5,41 @@ const seachForm = document.querySelector('#apiForm');
 const searchTitle = document.querySelector('#title');
 const searchYear = document.querySelector('#year');
 const searchActors = document.querySelector('#actors');
-const apiSearchReq = searchInput.value 
+const searchImg = document.querySelector('#searchImg');
+
 
 // We are only going to search by title for now to keep it simple, therefore need to amend our api req with t at the end. 
-
-
-
-
 
 seachForm.addEventListener('submit', function(e) {
     e.preventDefault();
     // setting a variable to be the search request that's input into the search box
     //now need to send this value to search the api, will now use the fetch API
-myFetch();
-   
+    const apiSearchReq = searchInput.value 
+    let resData;
+
+async function myFetch() {
+    let response = await fetch(`${URLSearch}${apiSearchReq}`);
+  
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    } else {
+      let resData = await response.json();
 
     
-//    let resData =  
-//   .then(response => response.json())
-//   .then(data => console.log(data));
+      searchTitle.innerHTML = resData.Title;
+      searchYear.innerHTML = resData.Year;
+      searchActors.innerHTML = resData.Actors;
+      searchImg.src = resData.Poster;
+      searchInput.value = "";
+      
+    }
+  }
+  myFetch()
+  .catch(e => {
+    console.log('There has been a problem with your fetch operation: ' + e.message);
+  });
+
+
 
 })
 
-async function myFetch() {
-    let response = await fetch(`${URLSearch}${apiSearchReq}`)
-
-    if(!response.ok){
-        throw new Error(`HTTP Error ${response.status}`)
-    } else {
-        let myResponse = await response.json();
-        console.log(myResponse)
-    }
-    
-}
